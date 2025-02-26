c8c01fa4c238a17d906dc54e809eae516f155fa4
Ammad-Official <ammadofficial786@gmail.com>
2025-02-26 14:19:46 +0500

    .

--- DataAccess/obj/Release/net6.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Release/net6.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9eb24fbe06ebea7675e6282ffebf899b24c5e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
